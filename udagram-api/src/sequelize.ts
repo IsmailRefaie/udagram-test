10aba5f88e57d3f23cd99dffa764395828efdd4f
IsmailRefaie <ismail_labib97@Yahoo.com>
2022-04-13 23:08:31 +0200

    update

--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -1,6 +1,8 @@
 import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
+console.log(config.username, config.password, config.database, config.host, config.port)
+
 export const sequelize = new Sequelize({
   username: config.username,
   password: config.password,
--- udagram-api/src/server.ts
+++ udagram-api/src/server.ts
@@ -37,3 +37,6 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
     console.log(`press CTRL+C to stop server`);
   });
 })();
+
+
+//&& zip -r Archive.zip . && cd ..
